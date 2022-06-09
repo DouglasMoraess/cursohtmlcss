decef87ef4c3b903e595c2c346e56a34df49fcce
Douglas de Moraes <86854119+DouglasMoraess@users.noreply.github.com>
2022-06-09 19:35:41 -0300

    .

--- javascript-cursoemvideo/aula14ex/scripts.js
+++ javascript-cursoemvideo/aula14ex/scripts.js
@@ -1,24 +1,27 @@
-function gerar(){
+// function gerar(){
 
 
-    let numb = document.getElementById('inica')
-    let tab = document.getElementById('setlab')
-    if (numb.value.length == 0 ){
-        alert(' Insira numero ')
-    }
-    else {
-        let n = Number(numb.value)
-        let c = 1
-        tab.innerHTML= ''
-        while(c<=10){
-            let item = document.createElement('option')
-            item.text = `${n} x ${c} = ${n*c}`
-            tab.appendChild(item)
-            c++
+//     let numb = document.getElementById('inica')
+//     let tab = document.getElementById('setlab')
+//     if (numb.value.length == 0 ){
+//         alert(' Insira numero ')
+//     }
+//     else {
+//         let n = Number(numb.value)
+//         let c = 1
+//         tab.innerHTML= ''
+//         while(c<=10){
+//             let item = document.createElement('option')
+//             item.text = `${n} x ${c} = ${n*c}`
+//             tab.appendChild(item)
+//             c++
 
-        }
-    }
+//         }
+//     }
 
 
 
-}
+// }
+
+
+let nome = prompt('Digite seu nome')
